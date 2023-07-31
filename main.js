da39e3519c534c37ead9b813f4583e0dbf751298
HavocDhanush <93704624+HavocDhanush@users.noreply.github.com>
2023-07-31 18:18:30 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+noseX = 0;
+noseY = 0;
+
+
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/g2sbJgnK/Clown-Nose-PNG-Image.png');
 
 }
 
@@ -16,8 +21,10 @@ function setup() {
 function gotPoses (results) {
     if (results.length >0) {
         console.log(results);
-        console.log("nose x = "+ results[0].pose.nose.x);
-        console.log("nose y = "+ results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-18;
+        noseY = results[0].pose.nose.y-15;
+        console.log("nose x = "+ noseX);
+        console.log("nose y = "+ noseY);
 
     }
 }
@@ -26,8 +33,15 @@ function modelLoaded () {
     console.log('PoseNet is initialized');
 }
 
+
+
 function draw() {
     image(video, 0, 0, 300, 300);
+    /* fill (255, 0, 0);
+    //stroke(255, 0, 0);
+    circle(noseX, noseY, 20); */
+    image(clown_nose, noseX, noseY, 40, 40);
+
 }
 
 function take_snapshot () {
